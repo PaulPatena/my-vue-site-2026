85c2a4d35b98e9c56aceeea50c4b36a99aa50d6d
Paul Patena <paulmichaelpatena@gmail.com>
2025-11-11 18:53:07 +1100

    feat: Add GitHub Pages deployment workflow and update Vite config for base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,17 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+const repoName = 'my-vue-site-2026'
+const basePath = process.env.GITHUB_ACTIONS ? `/${repoName}/` : '/'
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  // Use the repo name as base when deployed to GitHub Pages
+  base: basePath,
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
